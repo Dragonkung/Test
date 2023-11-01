1521e2463f216f6e0e5bd9a3f9c4ea4fed9469d5
Black <99789748+Dragonkung@users.noreply.github.com>
2023-11-01 10:18:15 +0700

    Create BypassTo.lua

---
+++ BypassTo.lua
@@ -0,0 +1,23 @@
+if game.PlaceId == 7449423635 then
+
+game.Players.LocalPlayer.Character.Head:Destroy()
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5226.99267578125, 313.7655029296875, -2932.35791015625)
+wait(0.1)
+--waitkoko
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5051.0859375, 314.5155029296875, -3003.775634765625)
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5051.0859375, 314.5155029296875, -3003.775634765625)
+wait(0.1)
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5020.4072265625, 554.6510009765625, -3017.13818359375)
+wait(4.8)
+--kk
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4951.0751953125, 314.5155029296875, -3174.642822265625)
+wait(0.1)
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4920.06103515625, 371.31292724609375, -2844.16357421875)
+wait(0.2)
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5053.20654296875, 314.5155029296875, -2987.9853515625)
+wait(0.1)
+Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5041.2099609375, 314.5155029296875, -2995.377197265625)
+
+else loadstring(game:HttpGet(("https://pastebin.com/raw/1DydNT4g"))()
+end
+
